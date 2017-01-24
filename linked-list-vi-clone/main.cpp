6658f3f1cd4de8af4617138680d7e7aa35f8c99a
Alexander Cochrane <acochrane@almightycorp.com>
2017-01-24 10:41:09 -0400

    Initial Commit

---
+++ linked-list-vi-clone/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  linked-list-vi-clone
+//
+//  Created by Alexander Cochrane on 2017-01-24.
+//  Copyright © 2017 Alexander Cochrane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
